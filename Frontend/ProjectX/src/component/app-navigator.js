d6b2ce10164bfb0288775b0f64018783fa29a98f
Piyush Jain <piyu905@gmail.com>
2017-12-25 23:23:21 +0530

    Added files

---
+++ Frontend/ProjectX/src/component/app-navigator.js
@@ -0,0 +1,10 @@
+import React from "react"
+//import { connect } from "react-redux"
+import Login from "./login"
+
+export default AppNavigator = props => {
+  if (true)
+    return (<Login />)
+}
+
+//const isWaiting = state => isWaitingNetwork(state)
